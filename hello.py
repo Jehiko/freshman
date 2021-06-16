3848b399681ea3fc93e07eaea58052fe8a3b348e
Jehiko <85975565+Jehiko@users.noreply.github.com>
2021-06-16 11:31:04 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 ## https://hub.gke2.mybinder.org/user/krischer-seismo_live_build-pmiaz5oh/notebooks/Signal%20Processing/filter_basics.ipynb
 
+# Cell 0 - Preparation: load packages, set some basic options  
+%matplotlib inline
+from obspy import *
+from obspy.clients.fdsn import Client
+import numpy as np
+import matplotlib.pylab as plt
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = 15, 4
+plt.rcParams['lines.linewidth'] = 0.5
+
 #Cell 1: prepare data from Tohoku earthquake. 
 client = Client("BGR")
 t1 = UTCDateTime("2011-03-11T05:00:00.000")
